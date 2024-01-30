85a21af38a53d206cdd6f9fc80769f825012a4dd
Chethana Gopinath <cgopina1@binghamton.edu>
2024-01-29 19:26:26 -0800

    [feat] basic setup

--- app.py
+++ app.py
@@ -9,25 +9,35 @@ layout = "centered"
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title("Tasks tracker " + page_icon)
 
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
 task_name = st.text_input('Task name', 'The actual task')
 task_due_date = st.date_input("When is this due?", datetime.date.today())
 notes = st.text_area("Notes", "Additional details and links about task")
 
-"---"
-
 submitted = st.button("Save task")
 if submitted:
-    # TODO: insert into database
-    st.write(f"Task {task_name} saved for {task_due_date}")
+    # Insert into database
+    doc_ref = db.collection("tasks").document(task_name)
+    doc_ref.set({
+		"name": task_name,
+		"due_date": task_due_date,
+        "notes": notes
+	})
     st.success("Data saved!")
 
+"---"
 
-# Authenticate to Firestore with the JSON account key.
-db = firestore.Client.from_service_account_json("firestore-key.json")
+# Show all tasks
+tasks_ref = db.collection("tasks")
+for doc in tasks_ref.stream():
+    task = doc.to_dict()
+    task_name = task["name"]
+    task_due_date = task["due_date"]
+    task_notes = task["notes"]
 
-
-doc_ref = db.collection("tasks").document("tasks-doc")
-doc = doc_ref.get()
-
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
+    st.subheader(task_name)
+    st.write(f"due by {task_due_date}")
+    with st.expander("Notes"):
+        st.write(task_notes)
