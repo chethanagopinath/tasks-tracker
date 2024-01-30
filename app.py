5d67798f06a63222848fbb70fd87572cf486dcf3
Chethana Gopinath <cgopina1@binghamton.edu>
2024-01-29 19:42:46 -0800

    [fix] fix to send as strs to firebase

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ st.title("Tasks tracker " + page_icon)
 db = firestore.Client.from_service_account_json("firestore-key.json")
 
 task_name = st.text_input('Task name', 'The actual task')
-task_due_date = st.date_input("When is this due?", datetime.date.today())
+task_due_date = st.date_input("Which date is this due?", datetime.date.today())
+task_due_time = st.time_input(f"When is this is due on {task_due_date}?", datetime.datetime.now())
 notes = st.text_area("Notes", "Additional details and links about task")
 
 submitted = st.button("Save task")
@@ -22,7 +23,8 @@ if submitted:
     doc_ref = db.collection("tasks").document(task_name)
     doc_ref.set({
 		"name": task_name,
-		"due_date": task_due_date,
+		"due_date": str(task_due_date),
+        "due_time": str(task_due_time),
         "notes": notes
 	})
     st.success("Data saved!")
@@ -35,6 +37,7 @@ for doc in tasks_ref.stream():
     task = doc.to_dict()
     task_name = task["name"]
     task_due_date = task["due_date"]
+    task_due_time = task["due_time"]
     task_notes = task["notes"]
 
     st.subheader(task_name)
