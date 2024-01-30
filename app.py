34e8771e50ec58340a841facf7b3d9e28e5e8d2c
Chethana Gopinath <cgopina1@binghamton.edu>
2024-01-29 20:38:15 -0800

    [feat] move into new navigation area

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-import streamlit as st
 import datetime
 from google.cloud import firestore
+
+import streamlit as st
+from streamlit_option_menu import option_menu
 
 page_title = "My personal tasks tracker"
 page_icon = "🗒"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
@@ -9,38 +11,47 @@ layout = "centered"
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title("Tasks tracker " + page_icon)
 
+# Navigation menu
+selected = option_menu(
+    menu_title=None,
+    options=["Add task", "See all tasks"],
+    icons=["plus", "list-task"],
+    orientation="horizontal",
+)
+
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestore-key.json")
 
-task_name = st.text_input('Task name', 'The actual task')
-task_due_date = st.date_input("Which date is this due?", datetime.date.today())
-task_due_time = st.time_input(f"When is this is due on {task_due_date}?", datetime.datetime.now())
-notes = st.text_area("Notes", "Additional details and links about task")
 
-submitted = st.button("Save task")
-if submitted:
-    # Insert into database
-    doc_ref = db.collection("tasks").document(task_name)
-    doc_ref.set({
-		"name": task_name,
-		"due_date": str(task_due_date),
-        "due_time": str(task_due_time),
-        "notes": notes
-	})
-    st.success("Data saved!")
+if selected == "Add task":
+    task_name = st.text_input('Task name', 'The actual task')
+    task_due_date = st.date_input("Which date is this due?", datetime.date.today())
+    task_due_time = st.time_input(f"When is this is due on {task_due_date}?", datetime.datetime.now())
+    notes = st.text_area("Notes", "Additional details and links about task")
 
-"---"
+    submitted = st.button("Save task")
+    if submitted:
+        # Insert into database
+        doc_ref = db.collection("tasks").document(task_name)
+        doc_ref.set({
+            "name": task_name,
+            "due_date": str(task_due_date),
+            "due_time": str(task_due_time),
+            "notes": notes
+        })
+        st.success("Data saved!")
 
-# Show all tasks
-tasks_ref = db.collection("tasks")
-for doc in tasks_ref.stream():
-    task = doc.to_dict()
-    task_name = task["name"]
-    task_due_date = task["due_date"]
-    task_due_time = task["due_time"]
-    task_notes = task["notes"]
+if selected == "See all tasks":
+    # Show all tasks
+    tasks_ref = db.collection("tasks")
+    for doc in tasks_ref.stream():
+        task = doc.to_dict()
+        task_name = task["name"]
+        task_due_date = task["due_date"]
+        task_due_time = task["due_time"]
+        task_notes = task["notes"]
 
-    st.subheader(task_name)
-    st.write(f"due by {task_due_date}")
-    with st.expander("Notes"):
-        st.write(task_notes)
+        st.subheader(task_name)
+        st.write(f"due by {task_due_date}")
+        with st.expander("Notes"):
+            st.write(task_notes)
